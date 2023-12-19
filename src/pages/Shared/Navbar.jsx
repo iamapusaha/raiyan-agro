58f6d031eb1d5f19eedd0f31e12ae0cf06e4ad58
Arju Mone <121635205+Arjumone@users.noreply.github.com>
2023-12-19 18:35:20 +0600

    added navbar section

---
+++ src/pages/Shared/Navbar.jsx
@@ -0,0 +1,42 @@
+import { NavLink } from "react-router-dom";
+
+
+const Navbar = () => {
+    const navLinks =(
+        <>
+        <NavLink><li>Home</li></NavLink>
+        <NavLink><li>Contact</li></NavLink>
+        <NavLink><li>Menu</li></NavLink>
+        {/* <NavLink><li>Sign In</li></NavLink>
+        <NavLink><li>Sign Up</li></NavLink> */}
+        </>
+    )
+    return (
+        <div>
+            <div className="navbar bg-base-100">
+            <div className="navbar-start hidden lg:flex ">
+    <ul className="menu menu-horizontal px-1 gap-6">
+      {navLinks}
+    </ul>
+  </div>
+  <div className="navbar-center">
+    <div className="dropdown">
+      <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+      </div>
+      <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+       {navLinks}
+      </ul>
+    </div>
+    <a className="btn btn-ghost text-xl">Raiyan Agro</a>
+  </div>
+  
+  <div className="navbar-end">
+    <a className="btn">Login</a>
+  </div>
+</div>
+        </div>
+    );
+};
+
+export default Navbar;
